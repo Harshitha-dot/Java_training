815869b64706bf6c482b2f97b61aac7b6100ac91
GudemeRamana <ramgudeme143@gmail.com>
2024-06-26 17:27:55 +0530

    firstcommit

---
+++ src/programs/FirstClass.java
@@ -0,0 +1,23 @@
+package programs;
+
+
+
+public class FirstClass {
+	
+
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	System.out.println("this is harshitha");
+
+
+
+
+
+		}
+
+	}
+
+
+
